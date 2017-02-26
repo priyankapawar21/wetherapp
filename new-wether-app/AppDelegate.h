04c5c044141b93d427bc14877d30b52d5e4bf111
Mac <mac@Macs-iMac.local>
2017-02-26 16:22:49 +0530

    Initial Commit

---
+++ new-wether-app/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  new-wether-app
+//
+//  Created by Mac on 26/02/17.
+//  Copyright © 2017 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
